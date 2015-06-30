a6ddf949e1d3ee5206ac88223a10135b64867886
mthomas2992 <mthomas2992@gmail.com>
2015-06-30 17:05:14 +1000

    Added rocket animation
    Also the relevent sprite sheets created/ older assets removed from unity
    project

--- Orbit/Assets/Player.cs
+++ Orbit/Assets/Player.cs
@@ -2,33 +2,27 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-	public Vector2 y = new Vector2 (1,1);
+	public Vector2 y = new Vector2 (0,0.1f);
 	private Vector2 mousePosition;
 	public float maxGravDist = 100f;
 	public float maxGravity = 0.5f;
-	private Vector3 initial;
 	//public float gravitational=0.0000000000667f;
 	GameObject[] planets;
-	SpriteRenderer sr;
-	public Sprite rocket;
-	public Sprite power;
 	private int fuel;
-	
-	
+	Animator An;
+
 	// Use this for initialization
 	void Start () {
 		fuel = 200;
-		initial = transform.position;
-		sr = GetComponent<SpriteRenderer> ();
 		planets = GameObject.FindGameObjectsWithTag("Planet");
+		An=GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButton (0)&&fuel>0) { //when mouse is held down
-			//Debug.Log ("I am alive");
+			An.SetBool("Active",true);
 			fuel=fuel-1;
-			sr.sprite = power;
 			Rigidbody2D x = GetComponent<Rigidbody2D> ();
 			Vector3 diff = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
 			diff.Normalize ();
@@ -36,14 +30,12 @@ public class Player : MonoBehaviour {
 			transform.rotation = Quaternion.Euler (0f, 0f, rot_z - 90);
 			x.AddRelativeForce (y, ForceMode2D.Impulse);
 		} else {
-			sr.sprite = rocket;
+			An.SetBool("Active",false);
 		}
 	}
 	
 	void OnCollisionEnter2D(Collision2D coll){ //NEED TO CHANGE THIS TO PLANET OBJECTS AND OTHERS
 		Application.LoadLevel (Application.loadedLevel); //resets level 
-		//GameObject.Destroy (gameObject);
-		//GameObject.Instantiate (gameObject,initial,transform.rotation);
 	}
 	
 	void FixedUpdate () {
